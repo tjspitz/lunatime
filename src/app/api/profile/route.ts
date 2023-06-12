44f9306b8cb87b7695fec349cb5487d77183e41d
Tom Spitz <tjspitz1986@gmail.com>
2023-06-12 17:17:23 -0500

    Implement routes to get cycle dates, profile info by userId

---
+++ src/app/api/profile/route.ts
@@ -0,0 +1,25 @@
+import mongo from '../../../utils/dbConfig';
+import User from '../../../models/userModel';
+import { NextResponse, NextRequest } from 'next/server';
+
+export async function GET(req: NextRequest) {
+  try {
+    await mongo();
+    const { searchParams } = new URL(req.url);
+    const userId = searchParams.get('userId');
+    const dates = await User.findById(userId, '-dates');
+    return NextResponse.json(dates);
+  } catch (error) {
+    return NextResponse.json(error);
+  }
+}
+
+/*
+export async function POST(req: NextRequest, res: NextResponse) {
+
+}
+
+export async function PATCH(req: NextRequest, res: NextResponse) {
+
+}
+*/
