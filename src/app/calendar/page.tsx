e5bce6e32e2986717d5d4736733187caa96e33c8
Tom Spitz <tjspitz1986@gmail.com>
2023-06-13 22:42:45 -0500

    cleared comments out, typed cycle keys to make TS happy

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -6,6 +6,7 @@ export interface CycleDates {
     {
       _id: Date;
       cycle: {
+        [key: string]: Date;
         fStart: Date;
         fEnd: Date;
         pStart: Date;
@@ -49,32 +50,3 @@ export default async function CalendarPage() {
     </main>
   );
 }
-
-/*
-import React, { useState } from 'react';
-import Calendar from 'react-calendar';
-
-const datesToAddContentTo = [tomorrow, in3Days, in5Days];
-
-function tileContent({ date, view }) {
-  // Add class to tiles in month view only
-  if (view === 'month') {
-    // Check if a date React-Calendar wants to check is on the list of dates to add class to
-    if (datesToAddContentTo.find(dDate => isSameDay(dDate, date))) {
-      return 'My content';
-    }
-  }
-}
-
-function MyApp() {
-  const [value, setValue] = useState(new Date());
-
-  return (
-    <Calendar
-      onChange={onChange}
-      value={date}
-      tileContent={tileContent}
-    />
-  );
-}
-*/
