75a9310759c176fb026b8d0a047cd9908665aebd
Tom Spitz <tjspitz1986@gmail.com>
2023-06-13 22:43:34 -0500

    implement functions to style each range of dates that is pulled from the db typed things as much as was able left a comment re: the date-fns 'isWithinInterval' functionality

--- src/app/calendar/calendar.tsx
+++ src/app/calendar/calendar.tsx
@@ -3,27 +3,61 @@ import 'react-calendar/dist/Calendar.css';
 import styles from './styles.module.css';
 import Calendar from 'react-calendar';
 import { CycleDates } from './page';
-
-import { differenceInCalendarDays, isWithinInterval } from 'date-fns';
+import { isWithinInterval } from 'date-fns';
 import { useEffect, useState } from 'react';
 
-const isSameDay = (a: Date, b: Date) => differenceInCalendarDays(a, b) === 0;
+const isWithinRange = (date: Date, range: Date[]): boolean => {
+  /*
+   * seems like a workaround to a bug that should not exist...
+   * backdating the start date by one day,
+   * since if we start at the initial range - range[0],
+   * isWithinInterval will not validate range[0],
+   * this despite a lot of time invested trying to prove that
+   * starting at range[0] was correctly following date-fns documentation
+   * as well as other resources
+   * tl;dr - the following 2 lines make it work as desired
+   */
+  const start = new Date(range[0]);
+  start.setDate(start.getDate() - 1);
 
-const isWithinRange = (date, range) => {
-  return isWithinInterval(date, { start: range[0], end: range[range.length - 1] });
-}
+  return isWithinInterval(date, {
+    start,
+    end: range[range.length - 1],
+  });
+};
 
-const isWithinRanges = (date, ranges) => {
+const isWithinRanges = (date: Date, ranges: Date[][]): boolean => {
   return ranges.some((range) => isWithinRange(date, range));
 };
 
-const rangesToStyle = (cycleDates: CycleDates, start: string, end: string): [] => {
-  const ranges: [] = [];
+const findRangeBounds = (date: Date, ranges: Date[][]): string | undefined => {
+  let prop: string | undefined;
+  const roundedDate = `${date.getDate()}${date.getMonth()}${date.getFullYear()}`;
+  ranges.forEach((range) => {
+    let roundedFirstBound = `${range[0].getDate()}${range[0].getMonth()}${range[0].getFullYear()}`;
+    let roundedLastBound = `${range[1].getDate()}${range[1].getMonth()}${range[1].getFullYear()}`;
+    if (roundedDate === roundedFirstBound) {
+      console.log('hit');
+      prop = 'first';
+    }
+    if (roundedDate === roundedLastBound) {
+      prop = 'last';
+    }
+  });
+  return prop;
+};
+
+const rangesToStyle = (
+  cycleDates: CycleDates,
+  start: string,
+  end: string
+): Date[][] => {
+  const ranges: Date[][] = [];
   const { dates } = cycleDates;
 
   dates.forEach((entry) => {
     const { cycle } = entry;
-    const subRange: [] = [];
+    const subRange: Date[] = [];
 
     for (const key in cycle) {
       if (key === start) {
@@ -34,17 +68,22 @@ const rangesToStyle = (cycleDates: CycleDates, start: string, end: string): [] =
       }
     }
     ranges.push(subRange);
-  })
-
+  });
   return ranges;
 };
 
 export default function CalendarContainer({ dates }: { dates: CycleDates }) {
   const [cycleDates, setCycleDates] = useState(dates);
   const [value, setValue] = useState(new Date());
-  const [fertileRanges, setFertileRanges] = useState(rangesToStyle(dates, 'fStart', 'fEnd'));
-  const [pmsRanges, setPmsRanges] = useState(rangesToStyle(dates, 'pStart', 'pEnd'));
-  const [menstrualRanges, setMenstrualRanges] = useState(rangesToStyle(dates, 'mStart', 'mEnd'));
+  const [fertileRanges, setFertileRanges] = useState(
+    rangesToStyle(dates, 'fStart', 'fEnd')
+  );
+  const [pmsRanges, setPmsRanges] = useState(
+    rangesToStyle(dates, 'pStart', 'pEnd')
+  );
+  const [menstrualRanges, setMenstrualRanges] = useState(
+    rangesToStyle(dates, 'mStart', 'mEnd')
+  );
 
   const handleChange = (newDate: Date) => {
     console.log('selected range: ', newDate);
@@ -52,20 +91,22 @@ export default function CalendarContainer({ dates }: { dates: CycleDates }) {
     setValue(newDate);
   };
 
-  const tileClassName = ({ date, view }: { date: Date; view: String }) => {
+  const tileClassName = ({ date, view }: { date: Date; view: String }): string => {
     let style = 'min-h-[10vh]';
 
     if (view === 'month') {
       if (isWithinRanges(date, fertileRanges)) {
         style += ' ' + styles.fertile;
+        style += ' ' + styles[findRangeBounds(date, fertileRanges)] || '';
       }
       if (isWithinRanges(date, pmsRanges)) {
         style += ' ' + styles.pms;
+        style += ' ' + styles[findRangeBounds(date, pmsRanges)] || '';
       }
       if (isWithinRanges(date, menstrualRanges)) {
         style += ' ' + styles.menstrual;
+        style += ' ' + styles[findRangeBounds(date, menstrualRanges)] || '';
       }
-      // if ()
     }
     return style;
   };
