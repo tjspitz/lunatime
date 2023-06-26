d47012e10af41f77ce6b3685de2555fea316384a
Tom Spitz <tjspitz1986@gmail.com>
2023-06-26 15:18:28 -0500

    Modularize form component (make subcomponent) Implement part of handleProfileUpdate (save functionality) Still need to post/patch to the DB

--- src/app/profile/profile.tsx
+++ src/app/profile/profile.tsx
@@ -1,27 +1,46 @@
 'use client';
-import { ProfileInfo } from '../../utils/interfaces';
 import Image from 'next/image';
+import { ProfileInfo } from '../../utils/interfaces';
+import EditForm from './editProfile';
 import { useEffect, useState } from 'react';
 
-const ProfileContainer = ({
-  profile,
-  fiftyStates,
-}: {
-  profile: ProfileInfo;
-  fiftyStates: string[];
-}) => {
+const ProfileContainer = ({ profile }: { profile: ProfileInfo }) => {
   const [profileData, setProfileData] = useState(profile);
   const [editable, setEditable] = useState(false);
-  const { firstName, lastName, phone, email, pic } = profileData;
+  const { _id, firstName, lastName, phone, email, pic } = profileData;
   const { city, state, zip } = profileData.address;
 
   const picBase64 = Buffer.from(pic).toString('base64');
 
-  const handleUpdateProfile = () => null;
+  const handleUpdateProfile = (e: Event) => {
+    e.preventDefault();
+    setEditable(false);
+    // set the Profile Data state (so no GET needed)
+    const form = e.target as HTMLFormElement;
+    const formData = new FormData(form);
+    const formJson = Object.fromEntries(formData.entries());
+
+    setProfileData({
+      _id,
+      firstName,
+      lastName,
+      phone: String(formJson.phone),
+      email: String(formJson.email),
+      pic,
+      address: {
+        city: String(formJson.city),
+        state: String(formJson.state),
+        zip: Number(formJson.zip),
+      }
+    });
+    // TODO:
+    // send a POST/PATCH request
+
+  };
 
   return (
-    <main className="flex flex-col m-8 min-h-full">
-      <div>
+    <main className="flex m-8">
+      <div className="mx-8">
         <Image
           src={`data:image/png;base64,${picBase64}`}
           alt="User's profile pic"
@@ -30,7 +49,7 @@ const ProfileContainer = ({
           style={{ borderRadius: '50%' }}
         />
         <Image
-          src="/editProfile.png"
+          src="/edit.png"
           alt="Edit your profile"
           width={50}
           height={50}
@@ -38,75 +57,19 @@ const ProfileContainer = ({
           onClick={() => setEditable(!editable)}
         />
       </div>
-      {/* <div className="py-4">
-        <p className="text-lg py-2">{firstName}&nbsp;{lastName}</p>
-        <p className="text-med py-2">{phone}</p>
-        <p className="text-med py-2">{email}</p>
-        <p className="text-base py-2">
-          {city}&#44;&nbsp;{state}&nbsp;{zip.toString()}
-          </p>
-      </div>
-      <div className="py-4">
-        <p className="text-lg py-2">
-          {firstName}&nbsp;{lastName}
-        </p>
-      </div> */}
       <div>
         <p className="text-lg py-2">
           {firstName}&nbsp;{lastName}
         </p>
-        <form className="py-4 flex flex-col" onSubmit={handleUpdateProfile}>
-          <label className="text-med py-2">
-            Phone:{' '}
-            <input name="phone" defaultValue={phone} disabled={!editable} />
-          </label>
-          <label className="text-med py-2">
-            Email:{' '}
-            <input name="email" defaultValue={email} disabled={!editable} />
-          </label>
-          <label className="text-med py-2">
-            City: <input name="city" defaultValue={city} disabled={!editable} />
-          </label>
-          <label className="text-med py-2">
-            State:
-            <select
-              name="state"
-              value={state}
-              onChange={() => null} // TODO
-              disabled={!editable}
-            >
-              {fiftyStates.map((state, i) => (
-                <option key={state + (i + 1)} value={state}>
-                  {state}
-                </option>
-              ))}
-            </select>
-          </label>
-          <label className="text-med py-2">
-            Zip:{' '}
-            <input
-              name="zip"
-              defaultValue={zip.toString()}
-              disabled={!editable}
-            />
-          </label>
-          {editable && (
-            <div className="flex flex-row justify-end text-med">
-              <button
-                className="p-2 mx-4 rounded-md border-solid border-2 border-red-400"
-                type="reset"
-              >
-                Cancel
-              </button>
-              <button
-                className="p-2 rounded-md border-solid border-2 border-red-400"
-                type="submit"
-              >
-                Save
-              </button>
-            </div>
-          )}
-        </form>
+        <EditForm
+          phone={phone}
+          email={email}
+          city={city}
+          state={state}
+          zip={zip}
+          editable={editable}
+          handleUpdateProfile={handleUpdateProfile}
+        />
       </div>
     </main>
   );
