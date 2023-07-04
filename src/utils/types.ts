6f6bac5432a01d7f50115eccc02ec65631219f41
Tom Spitz <tjspitz1986@gmail.com>
2023-07-04 14:23:58 -0500

    ReqOptions definition to expand

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,7 +1,11 @@
 export type PatchProfile = (
   id: string,
-  data: FormData
-  // data: {
-  //   [k: string]: FormDataEntryValue;
-  // }
+  // data: FormData
+  data: {
+    [k: string]: FormDataEntryValue;
+  }
 ) => Promise<any>;
+
+export type ReqOptions = {
+  [k: string]: RequestCache;
+}
