f3e16a886e6e68af2606a30ffa474aaeeaa82b27
Tom Spitz <tjspitz1986@gmail.com>
2023-07-27 10:36:54 -0500

    Implement range finder & its state, effect

--- src/app/components/CalEventModal.tsx
+++ src/app/components/CalEventModal.tsx
@@ -1,30 +1,67 @@
 'use client';
-import Modal from 'react-modal';
-import { Dispatch, SetStateAction } from 'react';
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
 import { CycleDates } from '@/utils/types';
 import { defaultStyles } from '@/utils/defaultStyles';
+import Modal from 'react-modal';
 
 Modal.setAppElement('#event-modal');
 
 const CalEventModal = ({
-  choseDate,
+  date,
+  cycleDates,
+  setCycleDates,
   showEventModal,
   setShowEventModal,
+  isWithinRange,
 }: {
-  choseDate: Date;
+  date: Date;
+  cycleDates: CycleDates;
+  setCycleDates: Dispatch<SetStateAction<CycleDates>>;
   showEventModal: boolean;
   setShowEventModal: Dispatch<SetStateAction<boolean>>;
+  isWithinRange: (date: Date, range: Date[]) => boolean;
 }) => {
+  const [currentRange, setCurrentRange] = useState<Date[]>([]);
+  useEffect(() => setCurrentRange(findRange()), [date])
+  /*
+    [x] detect specific date range that `date` resides in
+      [x] search corresponding props
+      [x] once found, apply that specific range to a state
+    [] supply that state (range) to a slider
+      [] adjust the slider to edit the range itself
+      [] save changes (PATCH)
+  */
+  const findRange = (): Date[] => {
+    const today = date;
+    const { dates } = cycleDates;
+    for (let i = 0; i < dates.length; i ++) {
+      const cycle = dates[i].cycle;
+      if (today > cycle.mEnd) {
+        continue;
+      }
+      if (isWithinRange(today, [cycle.fStart, cycle.fEnd])) {
+        return [cycle.fStart, cycle.fEnd];
+      }
+      if (isWithinRange(today, [cycle.pStart, cycle.pEnd])) {
+        return [cycle.pStart, cycle.pEnd];
+      }
+      if (isWithinRange(today, [cycle.mStart, cycle.mEnd])) {
+        return [cycle.mStart, cycle.mEnd];
+      }
+    }
+    return [];
+  };
+
   return (
     <div className="m-8 p-8 flex justify-center items-center">
       <Modal
         isOpen={showEventModal}
         onRequestClose={() => setShowEventModal(false)}
-        overlayClassName="bg-[rgba(0,0,0,.4] flex justify-center items-center absolute top-0 left-0 h-screen w-screen"
-        className="w-3/4 border-2 border-black bg-white rounded-xl p-8"
+        overlayClassName={defaultStyles.modalOverlay}
+        className={defaultStyles.modalStyle}
       >
         <h1 className="text-xl mb-6">Here&apos;s an event (modal)...</h1>
-        <p>Event on {choseDate.toDateString()}</p>
+        <p>Event on {date.toDateString()}</p>
       </Modal>
     </div>
   );
