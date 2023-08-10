4087cca01994d3d0b6ba1e92d93488c11cbd0ea1
Tom Spitz <tjspitz1986@gmail.com>
2023-08-10 15:39:50 -0500

    more useful styles

--- src/utils/defaultStyles.js
+++ src/utils/defaultStyles.js
@@ -1,8 +1,9 @@
 export const defaultStyles = {
   button: ' border-solid border-2 bg-red-100 rounded-md border-red-400 p-2 m-2',
-  hoverSm: ' hover:scale-105 transition-all ease-in-out duration-200',
+  hoverSm: ' hover:scale-110 transition-all ease-in-out duration-200',
+  hoverMed: ' hover:scale-125 transition-all ease-in-out duration-500',
   primaryBg: ' bg-gradient-to-b from-yellow-50 to-yellow-200',
   invertedPrimaryBg: ' bg-gradient-to-b from-yellow-200 to-yellow-50',
   modalOverlay: ' bg-[rgba(0,0,0,.4] flex justify-center items-center absolute top-0 left-0 h-screen w-screen',
-  modalStyle: 'w-3/4 border-2 border-black bg-white rounded-xl p-8',
+  modalStyle: ' w-3/4 border-2 border-black bg-white rounded-xl p-8',
 };
