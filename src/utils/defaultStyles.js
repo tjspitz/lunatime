64dee99366fbded77757bd7783d9cceed28daf98
Tom Spitz <tjspitz1986@gmail.com>
2023-08-23 10:56:00 -0500

    modal div style to faciliate hiding modal divs

--- src/utils/defaultStyles.js
+++ src/utils/defaultStyles.js
@@ -4,6 +4,7 @@ export const defaultStyles = {
   hoverMed: ' hover:scale-125 transition-all ease-in-out duration-500',
   primaryBg: ' bg-gradient-to-b from-yellow-50 to-yellow-200',
   invertedPrimaryBg: ' bg-gradient-to-b from-yellow-200 to-yellow-50',
+  modalDiv: '"m-8 p-8 flex justify-center items-center',
   modalOverlay: ' bg-[rgba(0,0,0,.4] flex justify-center items-center absolute top-0 left-0 h-screen w-screen',
   modalStyle: ' w-3/4 border-2 border-black bg-white rounded-xl p-8',
 };
