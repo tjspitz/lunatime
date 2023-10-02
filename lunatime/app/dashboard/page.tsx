b516ee57e78c307edb888d6f649668adb0546f1b
Tom Spitz <tjspitz1986@gmail.com>
2023-10-02 15:41:19 -0500

    Something to display upon successful signup/in

---
+++ lunatime/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+export default function Dashboard() {
+  return (
+    <main>
+      <div className="flex items-center justify-center w-screen h-screen bg-pink-50">
+        <div className="w-full max-w-[600px] mx-auto">
+          <h1 className="my-4 text-giant text-pink-950">Look, a dashboard!</h1>
+        </div>
+      </div>
+    </main>
+  );
+}
