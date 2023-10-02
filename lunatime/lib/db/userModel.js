c4bb383a94a84f6204e96add073cce198a3b60b0
Tom Spitz <tjspitz1986@gmail.com>
2023-10-02 15:20:23 -0500

    more specificity, and fix the notesModel import

--- lunatime/lib/db/userModel.js
+++ lunatime/lib/db/userModel.js
@@ -1,23 +1,23 @@
 import mongoose from 'mongoose';
 import cycleSchema from './cycleModel.js';
-import notesSchema from './notesSchema.js';
+import notesSchema from './notesModel.js';
 
 const userSchema = new mongoose.Schema({
   createdAt: { type: Date, default: Date.now },
   lastEdited: { type: Date, default: Date.now },
-  firstName: String,
-  lastName: String,
-  phone: String,
-  email: String,
-  username: String,
-  password: String,
-  pic: Buffer,
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  phone: { type: String, required: false },
+  email: { type: String, required: true },
+  username: { type: String, required: false },
+  password: { type: String, required: true },
+  pic: { type: Buffer, required: false },
   cycleLength: { type: Number, default: 30 },
   menstrualLength: { type: Number, default: 5 },
   address: {
-    city: String,
-    state: String,
-    zip: Number,
+    city: { type: String, required: false },
+    state: { type: String, required: false },
+    zip: { type: Number, required: false },
   },
   dates: [cycleSchema],
   notes: [notesSchema],
