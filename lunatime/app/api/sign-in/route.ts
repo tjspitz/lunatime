908103b07ced5c3f7292a19a8ef8b72ecbf0f098
Tom Spitz <tjspitz1986@gmail.com>
2023-11-03 08:59:36 -0500

    Implement proper signIn, signUp via JWY and cookie TODO is giving JWT the proper redirect Leaving cruft in to record all the broken ideas

--- lunatime/app/api/sign-in/route.ts
+++ lunatime/app/api/sign-in/route.ts
@@ -1,36 +1,77 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { NextRequest, NextResponse } from 'next/server';
 import User from '@/lib/db/userModel';
 import { comparePwds, createJWT } from '@/lib/auth';
 import { serialize } from 'cookie';
 
-// TO-DO: convert to NexRequest, NextRespone (APP)
-// currently using (PAGES) types
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
-  const user = await User.findOne(req.body.email);
+export async function POST(req: NextRequest, res: NextResponse) {
+  const body = await req.json();
+
+  const user = await User.findOne({ email: body.email });
 
   if (!user) {
-    res.status(401);
-    res.json({ error: 'User not found' });
+    res = NextResponse.json(
+      {
+        body: { 'data': 'User does not exist' },
+        success: false,
+      },
+      {
+        status: 402,
+        headers: { 'content-type': 'application/json' },
+      },
+    )
+    return res;
+    // res.status(401);
+    // res.json({ error: "Invalid login" });
+    // return NextResponse.json({ error: 'User not found' }, { status: 401 });
   }
 
-  const isUser = await comparePwds(req.body.password, user?.password as string);
+  const isUser = await comparePwds(body.password, user.password);
 
   if (isUser) {
     const jwt = await createJWT(user);
 
-    res.setHeader(
-      'Set-Cookie',
-      serialize(process.env.COOKIE_NAME as string, jwt, {
-        httpOnly: true,
-        path: '/',
-        maxAge: 604800,
-      }),
-    );
-    res.status(201);
-    res.json({});
+    // res.setHeader(
+    //   "Set-Cookie",
+    //   serialize(process.env.COOKIE_NAME as string, jwt, {
+    //     httpOnly: true,
+    //     path: "/",
+    //     maxAge: 60 * 60 * 24 * 7,
+    //   })
+    // );
+
+    // const newHeaders = new Headers(res.headers);
+    // newHeaders.set(
+    //   'Set-Cookie',
+    //   serialize(process.env.COOKIE_NAME as string, jwt, {
+    //     httpOnly: true,
+    //     path: '/',
+    //     maxAge: 604800,
+    //   }),
+    // );
+
+
+    res = NextResponse.json(
+      { success: true },
+      { status: 201, headers: { 'content-type': 'application/json' } }
+    )
+
+    res.cookies.set(process.env.COOKIE_NAME as string, jwt, {
+      httpOnly: true,
+      path: '/', // TODO
+      maxAge: 604800,
+    });
+
+
+
+    return res;
+    // res.status(201);
+    // res.end();
   } else {
-    res.status(401);
-    res.json({ error: 'Username and password do not match' });
-    res.json({});
+    // res.status(401);
+    // res.json({ error: "Username and password do not match" });
+    return NextResponse.json(
+      { error: 'Username and password do not match' }, { status: 401 }
+    );
   }
 }
--- lunatime/app/api/sign-up/route.ts
+++ lunatime/app/api/sign-up/route.ts
@@ -1,12 +1,29 @@
+// import { NextApiRequest, NextApiResponse } from 'next';
 import { NextRequest, NextResponse } from 'next/server';
-import { NextApiResponse } from 'next';
 import mongo from '@/lib/db/dbConfig';
 import User from '@/lib/db/userModel';
+import Cycle from '@/lib/db/cycleModel';
 import { createJWT, hashPwd } from '@/lib/auth';
 import { serialize } from 'cookie';
 
 export async function POST(req: NextRequest, res: NextResponse) {
   const body = await req.json();
+
+  /* Create/embed the first 'date' obj
+
+    body.lastPeriod: date last one started
+      // calulate its end based on body.menstrualLength
+
+    body.dates: await Cycle.create({
+      cycleLength: body.cycleLength,
+      menstrualLength: body.menstrualLength,
+      fertileRange: <...>,
+      pmsRange: <...>,
+      menstrualRange: [body.lastPeriod, (body.lastPeriod + body.menstrualLength)],
+
+    })
+
+  */
 
   try {
     await mongo();
@@ -18,31 +35,66 @@ export async function POST(req: NextRequest, res: NextResponse) {
     const user = await User.create({
       firstName: body.firstName,
       lastName: body.lastName,
-      // phone: body.phone,
       email: body.email,
-      // username: body.username,
       password: await hashPwd(body.password),
-      // pic: body.pic,
-      // cycleLength: body.cycleLength,
-      // menstrualLength: body.menstrualLength,
-      // address: body.address,
+      cycleLength: body.cycleLength,
+      menstrualLength: body.menstrualLength,
     });
 
     const jwt = await createJWT(user);
 
-    const newHeaders = new Headers(req.headers);
-    newHeaders.set(
-      'Set-Cookie',
-      serialize(process.env.COOKIE_NAME as string, jwt, {
-        httpOnly: true,
-        path: '/',
-        maxAge: 604800,
-      }),
+    // res.setHeader(
+    //   "Set-Cookie",
+    //   serialize(process.env.COOKIE_NAME as string, jwt, {
+    //     httpOnly: true,
+    //     path: "/",
+    //     maxAge: 60 * 60 * 24 * 7,
+    //   })
+    // );
+
+    // const newHeaders = new Headers(res.headers);
+    // newHeaders.set(
+    //   'Set-Cookie',
+    //   serialize(process.env.COOKIE_NAME as string, jwt, {
+    //     httpOnly: true,
+    //       path: '/',
+    //       maxAge: 604800,
+    //   }),
+    // );
+
+
+    // res.status(201);
+    // res.end();
+
+    res = NextResponse.json(
+      { success: true },
+      { status: 201, headers: { 'content-type': 'application/json' } }
     );
 
-    return NextResponse.json(res);
+    res.cookies.set(process.env.COOKIE_NAME as string, jwt, {
+      httpOnly: true,
+      path: '/', // TODO
+      maxAge: 604800,
+    });
+
+    return res;
+    // return NextResponse.json(res);
 
   } catch(error) {
-    console.error('Failed to complete Registration in POST request: \n', error);
+    console.error('error: ', error)
+    res = NextResponse.json(
+      {
+        body: { 'data': error },
+        success: false,
+      },
+      {
+        status: 401,
+        headers: { 'content-type': 'application/json' },
+      },
+    );
+    return res;
+    // res.status(402);
+    // res.end();
+    // console.error('Failed to complete Registration in POST request: \n', error);
   }
 }
