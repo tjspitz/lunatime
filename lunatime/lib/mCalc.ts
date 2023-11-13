8824863a1da8d913c99d3923dbb6df5e68359379
Tom Spitz <tjspitz1986@gmail.com>
2023-11-13 12:38:45 -0600

    TODO, implement helper fn for predicting next dates

---
+++ lunatime/lib/mCalc.ts
@@ -0,0 +1,24 @@
+/*
+To calculate when your next period will be:
+
+Measure how long your average cycle is, i.e., the time between one period beginning and the next one beginning.
+
+Note how long your average period duration is.
+
+Add the length of your cycle to the date your last period began. This is the day your next period should begin.
+
+Add the length of your period to the date your next period will begin. This is when your next period should end.
+
+*/
+
+/*
+Here's how to calculate your fertility window:
+
+Calculate your ovulation day. Take your average cycle duration, subtract 14 days from this value, and add the remaining number of days to the date your last period began.
+
+Sperm can survive inside a woman's body for 7 days, so subtract 7 days from your ovulation day. This is when your fertile window begins.
+
+On average, the egg can survive one day outside of the ovaries, so add 1 day to your ovulation day. This is the end of your fertile window.
+
+This time between these two dates is your fertile window.
+*/
