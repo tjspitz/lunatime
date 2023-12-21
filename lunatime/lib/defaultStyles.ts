25f24c7f786268017cee6d8fac38ad3a19f8c3bf
Tom Spitz <tjspitz1986@gmail.com>
2023-12-20 20:25:34 -0600

    remove duplicate file

--- lunatime/lib/defaultStyles.ts
+++ lunatime/lib/defaultStyles.ts
@@ -1,8 +1,10 @@
 export const defaultStyles = {
-  button: ' px-4 py-2 my-4 rounded-xl bg-pink-950 text-pink-50',
+  button: ' border-solid border-2 bg-red-100 rounded-md border-red-400 p-2 m-2',
   hoverSm: ' hover:scale-110 transition-all ease-in-out duration-200',
   hoverMed: ' hover:scale-125 transition-all ease-in-out duration-500',
-  modalDiv: ' m-8 p-8 flex justify-center items-center',
+  primaryBg: ' bg-gradient-to-b from-yellow-50 to-yellow-200',
+  invertedPrimaryBg: ' bg-gradient-to-b from-yellow-200 to-yellow-50',
+  modalDiv: '"m-8 p-8 flex justify-center items-center',
   modalOverlay: ' bg-[rgba(0,0,0,.4] flex justify-center items-center absolute top-0 left-0 h-screen w-screen',
   modalStyle: ' w-3/4 border-2 border-black bg-white rounded-xl p-8',
 };
