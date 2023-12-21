601651c6bb6f7725dc79bdaba7d2a73f0ca7191c
Tom Spitz <tjspitz1986@gmail.com>
2023-12-20 20:27:38 -0600

    migrate Cal Modals into v2 dir, all are TO-DO

---
+++ lunatime/components/CalActionModal.tsx
@@ -0,0 +1,150 @@
+'use client';
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
+ import { defaultStyles } from '@/lib/defaultStyles';
+import { CycleDates } from '@/lib/types';
+import { add } from 'date-fns';
+import Modal from 'react-modal';
+
+Modal.setAppElement('#cal-modal');
+
+export default function CalActionModal({
+  date,
+  cycleDates,
+  showActionModal,
+  setShowActionModal,
+  setShowNoteModal,
+  setShowEventModal,
+  fertileRanges,
+  pmsRanges,
+  menstrualRanges,
+  isWithinRanges,
+}: {
+  date: Date;
+  cycleDates: CycleDates;
+  showActionModal: boolean;
+  setShowActionModal: Dispatch<SetStateAction<boolean>>;
+  setShowNoteModal: Dispatch<SetStateAction<boolean>>;
+  setShowEventModal: Dispatch<SetStateAction<boolean>>;
+  fertileRanges: Date[][];
+  pmsRanges: Date[][];
+  menstrualRanges: Date[][];
+  isWithinRanges: (date: Date, ranges: Date[][]) => boolean;
+}) {
+  const { dates } = cycleDates;
+  const [finalDate, setFinalDate] = useState<Date>(new Date());
+
+  useEffect(() => {
+    const lastCycle = dates[dates.length - 1].cycle;
+    let lastFound;
+
+    if (lastCycle.mEnd) {
+      lastFound = lastCycle.mEnd;
+    } else if (lastCycle.pEnd) {
+      lastFound = lastCycle.pEnd;
+    } else if (lastCycle.fEnd) {
+      lastFound = lastCycle.fEnd;
+    } else {
+      lastFound = new Date();
+    }
+
+    setFinalDate(lastFound);
+  }, [dates]);
+
+  const handleChoice = (e: any): void => {
+    e.preventDefault();
+    const choice = e.target.innerText;
+    if (choice.includes('Note')) {
+      setShowNoteModal(true);
+    } else if (choice.includes('Event')) {
+      setShowEventModal(true);
+    }
+    setShowActionModal(false);
+  };
+
+  const showEventPrompt = (): boolean => {
+    if (date >= add(finalDate, { days: 7 })) {
+      return true;
+    }
+    if (
+      isWithinRanges(date, fertileRanges) ||
+      isWithinRanges(date, pmsRanges) ||
+      isWithinRanges(date, menstrualRanges)
+    ) {
+      return true;
+    }
+    return false;
+  };
+
+  return (
+    <div
+      className={`${defaultStyles.modalDiv} ${showActionModal ? '' : 'hidden'}`}
+    >
+      <Modal
+        isOpen={showActionModal}
+        onRequestClose={() => setShowActionModal(false)}
+        overlayClassName={defaultStyles.modalOverlay}
+        className={defaultStyles.modalStyle}
+      >
+        <h1 className="mb-6 text-xl">Would you like to...</h1>
+        <button
+          className={defaultStyles.button + defaultStyles.hoverSm}
+          onClick={handleChoice}
+        >
+          Add a Note
+        </button>
+        {showEventPrompt() && (
+          <button
+            className={defaultStyles.button + defaultStyles.hoverSm}
+            onClick={handleChoice}
+          >
+            Add or Edit an Event
+          </button>
+        )}
+      </Modal>
+    </div>
+  );
+}
+
+/*
+click on any given date...
+
+AS LONG AS THERE IS AT LEAST ONE COMPLETE CYCLE AVAILABLE
+  [x]if the current date is NOT within a cycle range AND it is LESS THAN the "oldest" possible date
+    prompt to enter a Note
+
+  if the current date is GREATER THAN the "oldest" possible date (or there are no dates yet)
+    prompt to enter a Note
+    prompt to start a new cycle
+      choose fertile, pms, or menstrual (default fertile)
+      if fertile is chosen
+        calculate entire cycle & build rest of object (based on past dates)
+      if pms or menstrual is chosen
+        calculate backdated parts & build rest of object (based on past dates)
+      when "save" is clicked, advise that the cycle will be estimated & edits can be made at any time
+
+  if the current date falls WITHIN a cycle range, detect WHICH range it is, and...
+    prompt to enter a Note
+    prompt to edit THAT particular range
+      generate a slider that will represent the start & end dates?
+      generate another date picker?
+    when "save" is clicked, ask if following cycles should be updated automatically
+      if yes, adjust all cycles by whatever parameters have been changed, then submit to the PATCH
+        i.e. start date moved back 2 days & end back 1 day, move all other starts back 2 days, ends back 1
+      if no, submit as-is to the PATCH
+
+THERE ARE NO COMPLETE CYCLES YET (NEW USER)
+  prompt to enter a Note
+
+  if the cycle object is EMPTY
+    prompt to start a new cycle
+      choose fertile, pms, or menstrual (default fertile)
+      assign current date as f/p/m/Start
+
+  if the cycle object is PARTIAL
+    if there is an f/p/m/Start with no End recorded
+      prompt to update corresponding f/p/m/End
+      (or) prompt to mark *other* start
+    if it is semi-complete i.e. fStart & fEnd exist but p/m do not
+      prompt to mark current date as nextThing/Start
+
+*/
---
+++ lunatime/components/CalEventModal.tsx
@@ -0,0 +1,191 @@
+'use client';
+import 'react-calendar/dist/Calendar.css';
+import Calendar from 'react-calendar';
+import Modal from 'react-modal';
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
+import { CycleDates, PutCycles, RangeData } from '@/lib/types';
+import { defaultStyles } from '@/lib/defaultStyles';
+Modal.setAppElement('#cal-modal');
+
+const defaultRange: RangeData = {
+  index: -1,
+  type: '',
+  range: [new Date(), new Date()],
+};
+
+export default function CalEventModal({
+  date,
+  cycleDates,
+  setCycleDates,
+  showEventModal,
+  setShowEventModal,
+  isWithinRange,
+}: {
+  date: Date;
+  cycleDates: CycleDates;
+  setCycleDates: Dispatch<SetStateAction<CycleDates>>;
+  showEventModal: boolean;
+  setShowEventModal: Dispatch<SetStateAction<boolean>>;
+  isWithinRange: (date: Date, range: Date[]) => boolean;
+}) {
+  const [value, setValue] = useState(date);
+  const [currentRange, setCurrentRange] = useState<RangeData>(defaultRange);
+  const [newRange, setNewRange] = useState<RangeData>(defaultRange);
+  const [newEventType, setNewEventType] = useState<string>('');
+
+  useEffect(() => setCurrentRange(findRange()), [date]);
+  useEffect(() => setNewRange(currentRange), [currentRange]);
+
+  const findRange = (): RangeData => {
+    const today = date;
+    const { dates } = cycleDates;
+    for (let i = 0; i < dates.length; i++) {
+      const cycle = dates[i].cycle;
+      if (today > cycle.mEnd) {
+        continue;
+      }
+      if (isWithinRange(today, [cycle.fStart, cycle.fEnd])) {
+        return {
+          index: i,
+          type: 'fertile',
+          range: [cycle.fStart, cycle.fEnd],
+        };
+      }
+      if (isWithinRange(today, [cycle.pStart, cycle.pEnd])) {
+        return {
+          index: i,
+          type: 'pre-menstrual',
+          range: [cycle.pStart, cycle.pEnd],
+        };
+      }
+      if (isWithinRange(today, [cycle.mStart, cycle.mEnd])) {
+        return {
+          index: i,
+          type: 'menstrual',
+          range: [cycle.mStart, cycle.mEnd],
+        };
+      }
+    }
+    return {} as RangeData;
+  };
+
+  const handleChange = (newDate: Date): void => {
+    setValue(newDate);
+    setNewRange({
+      index: currentRange.index,
+      type: currentRange.type || newEventType,
+      range: newDate, // TODO
+    });
+  };
+
+  const handleSaveClick = (e: any): void => {
+    e.preventDefault();
+    const i = newRange.index;
+    const start = newRange.type[0] + 'Start';
+    const end = newRange.type[0] + 'End';
+    const newCycles = { ...cycleDates };
+    newCycles.dates[i].cycle[start] = newRange.range[0];
+    newCycles.dates[i].cycle[end] = newRange.range[1];
+    putCycles(cycleDates._id, newCycles);
+    setCycleDates(newCycles);
+    setShowEventModal(false);
+  };
+
+  const handleModalClose = (e: any): void => {
+    e.preventDefault();
+    setNewRange(defaultRange);
+    setNewEventType('');
+    setShowEventModal(false);
+  };
+
+  const handleTypeClick = (e: any): void => {
+    e.preventDefault();
+    setNewEventType(e.target.innerText);
+  };
+
+  return (
+    <div
+      className={`${defaultStyles.modalDiv} ${showEventModal ? '' : 'hidden'}`}
+    >
+      <Modal
+        isOpen={showEventModal}
+        onRequestClose={handleModalClose}
+        overlayClassName={defaultStyles.modalOverlay}
+        className={defaultStyles.modalStyle}
+      >
+        <div className="flex justify-between">
+          <h1 className="mb-6 text-xl">Editing {date.toDateString()}...</h1>
+          <button
+            className={`self-start text-lg text-gray-500 ${defaultStyles.hoverMed} hover:text-red-400`}
+            onClick={handleModalClose}
+          >
+            X
+          </button>
+        </div>
+        {currentRange.type ? (
+          <>
+            <p className="mb-2 text-med">
+              Please choose a new range of dates for this {currentRange.type}{' '}
+              event:
+            </p>
+            <Calendar
+              className={'min-w-fit'}
+              calendarType="US"
+              view="month"
+              minDetail="month"
+              selectRange={true}
+              value={date} // need some styling a la tileClassName
+              onChange={handleChange}
+            />
+          </>
+        ) : (
+          <>
+            <p className="mb-2 text-med">
+              Creating a new {newEventType} event:
+            </p>
+            {['Fertility', 'Pre-Menstrual', 'Menstruation'].map((text, i) => (
+              <button
+                key={i + text}
+                className={`${defaultStyles.button} ${defaultStyles.hoverSm} focus:ring focus:ring-red-900`}
+                onClick={handleTypeClick}
+              >
+                {text}
+              </button>
+            ))}
+            <Calendar
+              className={'min-w-fit'}
+              calendarType="US"
+              view="month"
+              minDetail="month"
+              selectRange={false}
+              value={value}
+              onChange={handleChange}
+            />
+          </>
+        )}
+        <button
+          className={`mt-4 px-4 py-2 bg-blue-500 text-white rounded-md ${defaultStyles.hoverMed}`}
+          onClick={handleSaveClick}
+        >
+          Save
+        </button>
+      </Modal>
+    </div>
+  );
+}
+
+async function putCycles(id, dates): Promise<PutCycles> {
+  try {
+    const url = 'http://localhost:3000/api/calendar';
+    const params = new URLSearchParams({ userId: id });
+    const options = {
+      method: 'PUT',
+      body: JSON.stringify(dates),
+    };
+    const res = await fetch(`${url}?${params}`, options);
+    return res.json();
+  } catch (error: any) {
+    console.error(error);
+    return {} as CycleDates;
+  }
+}
